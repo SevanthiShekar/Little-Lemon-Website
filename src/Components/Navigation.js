03bd0462319c175c86747072967328e783978afa
Sevanthi Shekar <sevanthishekar2005@gmail.com>
2024-09-04 23:49:39 +0530

    HomePage Final Polishing

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import "./Navigation.css"
-import font from "./assets/MarkaziText-VariableFont_wght.ttf"
 import {Link} from "react-router-dom";
 
 const Navigation = () => {
   return (
     <nav>
         <ul>
-            <li><Link to="/" style={{color: "#333333", fontFamily: font}}>Home</Link></li>
+            <li><Link to="/" style={{color: "#333333"}}>Home</Link></li>
             <li><Link to="/about" style={{color: "#333333"}}>About</Link></li>
             <li><Link to="/menu" style={{color: "#333333"}}>Menu</Link></li>
             <li><Link to="/reservations" style={{color: "#333333"}}>Reservations</Link></li>
